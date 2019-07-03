f5c5d6ed4de00bdf10f7e5a885759924147d6dda
R. Garcia-Dias <rafaelagd@gmail.com>
2019-07-03 10:39:12 +0100

    Start looking into the data

--- data_explore/first_look.py
+++ data_explore/first_look.py
@@ -5,9 +5,9 @@ import pandas as pd
 from pandas_profiling import ProfileReport
 import seaborn as sns
 
-CONGRESSPEOPLE = pd.read_csv('%s/data/reimbursements.csv' % getcwd(), low_memory=False, encoding='utf_8',
-                             error_bad_lines=False, index_col=0)
-ProfileReport(CONGRESSPEOPLE).to_file('%s/reports/reimbursements.html' % (getcwd()))
+CONGRESSPEOPLE = pd.read_csv('%s/data_explore/data/reimbursements.csv' % getcwd(), low_memory=False,
+                             encoding='utf_8', error_bad_lines=False, index_col=0)
+# ProfileReport(CONGRESSPEOPLE).to_file('%s/reports/reimbursements.html' % (getcwd()))
 
 TOTAL_BY_YEAR = [CONGRESSPEOPLE[CONGRESSPEOPLE.year == year].groupby('congressperson_name')['document_value'].sum()
                  for year in range(2014, 2020)]
@@ -24,9 +24,13 @@ STD_BY_PARTY = TOTAL_BY_YEAR.groupby('party').mean()[list(range(2014, 2019))].so
 PARTY_CODES = [i for i, value in enumerate(MEAN_BY_PARTY.index)]
 f = sns.catplot(data=TOTAL_BY_YEAR.groupby('party').mean().sort_values(2018).T, kind="point", size=8)
 f.set_xticklabels(rotation=90)
-
 plt.figure(figsize=(16, 9))
 plt.errorbar(range(len(MEAN_BY_PARTY)), MEAN_BY_PARTY / 1e3, STD_BY_PARTY / 1e3, marker='', ls='')
 plt.scatter(range(len(MEAN_BY_PARTY)), MEAN_BY_PARTY / 1e3, c=PARTY_CODES, cmap=plt.cm.tab20)
 plt.xticks(labels=MEAN_BY_PARTY.index.astype(str), ticks=range(len(MEAN_BY_PARTY)), rotation=90)
 plt.show()
+
+CONGRESSPEOPLE.cnpj_cpf = CONGRESSPEOPLE.cnpj_cpf.astype('category')
+CONGRESSPEOPLE.document_value = CONGRESSPEOPLE.document_value.fillna(0.0)
+COMPANIES_BY_PARTY = CONGRESSPEOPLE.groupby(['party', 'supplier'])[['document_value']].sum()
+COMPANIES_BY_PARTY.loc['PSL'].sort_values(ascending=False, by='document_value')[:10].plot.bar()

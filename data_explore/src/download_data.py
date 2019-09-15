653c47a829dcd18760185bdacaf4e2d0888c9933
R. Garcia-Dias <rafaelagd@gmail.com>
2019-09-15 14:36:14 +0100

    Get the download running.

--- data_explore/src/download_data.py
+++ data_explore/src/download_data.py
@@ -11,16 +11,16 @@ def donload_data(year):
     return ChamberDataset(year, 'data/')
 
 
-for run_download in map(donload_data, range(2014, 2020)):
-    run_download()
-DATA_PATH = '%s/data' % getcwd()
-Path('%s/reports/' % getcwd()).mkdir(exist_ok=True)
+if __name__ == '__main__':
+    for run_download in map(donload_data, range(2014, 2020)):
+        run_download()
+    DATA_PATH = '%s/data' % getcwd()
+    Path('%s/reports/' % getcwd()).mkdir(exist_ok=True)
+    csv_files = list(Path(DATA_PATH).glob('*reimbursements*.csv'))
+    combined_data = map(lambda filepath: pd.read_csv(filepath, low_memory=False, encoding='utf_8',
+                                                     error_bad_lines=False, delimiter=','), csv_files)
+    combined_data = pd.concat(combined_data)
+    list(map(lambda filepath: filepath.unlink(), Path(DATA_PATH).glob('*')))
+    combined_data.to_csv('%s/reimbursements.csv' % DATA_PATH)
 
-csv_files = list(Path(DATA_PATH).glob('*reimbursements*.csv'))
-combined_data = map(lambda filepath: pd.read_csv(filepath, low_memory=False, encoding='utf_8',
-                                                 error_bad_lines=False, delimiter=','), csv_files)
-combined_data = pd.concat(combined_data)
-list(map(lambda filepath: filepath.unlink(), Path(DATA_PATH).glob('*')))
-combined_data.to_csv('%s/reimbursements.csv' % DATA_PATH)
-
-ProfileReport(combined_data).to_file('%s/reports/reimbursements.html' % (getcwd()))
+    ProfileReport(combined_data).to_file('%s/reports/reimbursements.html' % (getcwd()))
